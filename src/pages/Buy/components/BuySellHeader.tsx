e1362d782325c877c47b19b961beac231ee5ea35
Godswill Adigwe <gcadigwe@gmail.com>
2023-01-04 17:14:47 +0100

    buy-and-sell-screen

---
+++ src/pages/Buy/components/BuySellHeader.tsx
@@ -0,0 +1,96 @@
+import {
+  Flex,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Buy from "..";
+import BuySection from "./Buy";
+import SellSection from "./Sell";
+
+const BuySellHeader = () => {
+  const buyBgColor = useColorModeValue("#0CCB80", "");
+  const sellBgColor = useColorModeValue("#CC334F", "");
+  const borderColor = useColorModeValue("#F2F5F8", "");
+  const currencyTextColor = useColorModeValue("#666666", "");
+  const activeCurrencyColor = useColorModeValue("#319EF6", "");
+  return (
+    <Flex
+      px={3}
+      py={4}
+      w='100%'
+      //   border={"1px solid red"}
+    >
+      <Flex w='100%' justifyContent={"space-between"}>
+        <Tabs>
+          <TabList
+            w='100%'
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Tab
+              px={5}
+              borderRadius={"6px"}
+              border={`1px solid ${borderColor}`}
+              _selected={{ color: "white", bg: buyBgColor }}
+              fontWeight={"500"}
+            >
+              Buy
+            </Tab>
+            <Tab
+              fontWeight={"500"}
+              px={5}
+              borderRadius={"6px"}
+              ml={2}
+              border={`1px solid ${borderColor}`}
+              _selected={{ color: "white", bg: sellBgColor }}
+            >
+              Sell
+            </Tab>
+
+            <Flex mt={4} alignItems={"center"} ml={10}>
+              <Text
+                color={activeCurrencyColor}
+                borderBottom={`2px solid ${activeCurrencyColor}`}
+                px={3}
+              >
+                RGP
+              </Text>
+              <Text color={currencyTextColor} px={3}>
+                USDT
+              </Text>
+              <Text color={currencyTextColor} px={3}>
+                BNB
+              </Text>
+              <Text color={currencyTextColor} px={3}>
+                BUSD
+              </Text>
+            </Flex>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <BuySection />
+            </TabPanel>
+            <TabPanel>
+              <SellSection />
+            </TabPanel>
+          </TabPanels>
+          {/* <TabPanels>
+            <TabPanel>
+              <p>one</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels> */}
+        </Tabs>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default BuySellHeader;
--- src/pages/Buy/index.tsx
+++ src/pages/Buy/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import {Box} from "@chakra-ui/react"
-
+import { Box, Flex } from "@chakra-ui/react";
+import BuySellHeader from "./components/BuySellHeader";
 
 export default function Buy() {
   return (
-    <Box>Buy</Box>
-  )
+    <Flex py={5} minH={"100vh"} border={"1px solid"} justifyContent={"center"}>
+      <Flex
+        border={"1px solid #666666"}
+        borderRadius={"20px"}
+        w='460px'
+        h='600px'
+
+        // justifyContent={"center"}
+        // alignItems={"center"}
+      >
+        <BuySellHeader />
+      </Flex>
+    </Flex>
+  );
 }
